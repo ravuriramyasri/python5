951f6aa73c19c57128ad266e7c11124eba740d86
ravuriramyasri <42571523+ravuriramyasri@users.noreply.github.com>
2018-09-06 12:00:47 +0530

    Update Odd.py

--- Odd.py
+++ Odd.py
@@ -1,10 +1,6 @@
-try:
-	s=int(input());
-	e=int(input())
-	for i in range(s+1,e):
-		if i%2==0:
-			continue
-		print(i)
-except:
-	print('invalid')
-	
+c,d=map(int,raw_input().split())
+for i in range(a+1,b):
+	if i%2!=0:
+		print i
+
+    
