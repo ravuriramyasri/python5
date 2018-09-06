5cc5493db89feb1b8b4c9666726f4c8fb6061627
ravuriramyasri <42571523+ravuriramyasri@users.noreply.github.com>
2018-09-06 10:41:01 +0530

    Update Exponent.py

--- Exponent.py
+++ Exponent.py
@@ -1,8 +1,3 @@
-def power(base,exp):
-	if(exp==1):
-		return (base)
-	if(exp!=1):
-		return(base*power(base,exp-1))
-base=int(raw_input())
-exp=int(raw_input())
-print(power(base,exp))
+a,b=map(int,raw_input().split())
+c=a**b
+print c
