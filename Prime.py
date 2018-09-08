170ba59841cf03b406d33843c6a4cf72506a6345
ravuriramyasri <42571523+ravuriramyasri@users.noreply.github.com>
2018-09-08 12:59:32 +0530

    Update Prime.py

--- Prime.py
+++ Prime.py
@@ -1,9 +1,8 @@
-lower=int(raw-input())
-upper=int(raw_input())
-for num i in range(lower,upper):
-     if num>1:
-         for i in range(2,num):
-              if(num % i)==0:
-                 break
-         else:
-              print(num) 
+a,b=map(int,raw_input().split())
+for i in range(a+1,b):
+     if i>1:
+          for v in range(2,i):
+               if i%v==0:
+                    break
+          else:
+               print i,
