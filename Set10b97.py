59d0252444a6a126cebd6335519863e13c94ce08
ravuriramyasri <42571523+ravuriramyasri@users.noreply.github.com>
2018-12-13 11:11:33 +0530

    Update Set10b97.py

--- Set10b97.py
+++ Set10b97.py
@@ -2,11 +2,11 @@ def rev():
 N=int(input())
 rev=0
 while(N!=0):
-r=N%10
-rev=rev*10+r
-N//=10
-print(rev)
+  r=N%10
+  rev=rev*10+r
+  N//=10
+ print(rev)
 try:
-rev()
+ rev()
 except:
-print("invalid")
+ print("invalid")
