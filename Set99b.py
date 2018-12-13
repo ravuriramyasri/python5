bc073a7461bb606835441be0e15c5801589cb238
ravuriramyasri <42571523+ravuriramyasri@users.noreply.github.com>
2018-12-13 11:54:13 +0530

    Create Set99b.py

---
+++ Set99b.py
@@ -0,0 +1,21 @@
+import sys
+def st():
+        (a,b,c)=map(int,sys.stdin.readline().split())
+        print(a*b)%c)
+try:
+        st()
+except:
+        print('invalid')
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+            
