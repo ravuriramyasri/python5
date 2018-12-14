0ca3847cad25bde69cc938cf31933d6a8d507b21
ravuriramyasri <42571523+ravuriramyasri@users.noreply.github.com>
2018-12-14 11:22:33 +0530

    Update Fact.py

--- Fact.py
+++ Fact.py
@@ -1,8 +1,8 @@
-N=int(raw_input())
-if N==0:
-        print("factorial of zero is 1")
+n=int(raw_input())
+fact=1
+if(n==0):
+	print 1
 else:
-        fact=1
-        for i in range(1,N+1):
-                fact=fact*i
-        print(fact)
+	for i in range(1,n+1):
+		fact=fact*i
+	print fact
